b6f63d300a763d622c4cbbe156c8b17258e79d89
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-09-22 10:41:46 -0600

    fixes typo

--- Test/MyTest.c
+++ Test/MyTest.c
@@ -1,16 +1,17 @@
 //  rb headers
 #include "ruby.h"
 // variable stores the value
+// Qnil represents a ruby value
 VALUE MyTest = Qnil;
 
 void Init_mytest();
+// returns a ruby value
 VALUE method_test1(VALUE self);
 void Init_mytest() {
-
   MyTest = rb_define_module("MyTest");
   rb_define_method(MyTest, "test1", method_test1, 0);
 }
-VALUE method_test(VALUE self) {
+VALUE method_test1(VALUE self) {
   int x = 10;
   return INT2NUM(x);
 }
