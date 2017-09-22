1d617b42c0bbbbc7dd084de6c5d44b18aca8de3a
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-09-22 11:32:12 -0600

    adds half method array

--- Half/test/spec/half_spec.rb
+++ Half/test/spec/half_spec.rb
@@ -8,7 +8,12 @@ describe Half do
     end
   end
 
-  # context "when passed an array" do
-  #   it "returns two arrays of half the size"
-  # end
+  context "when passed an array" do
+    it "returns two arrays of half the size"
+    test1      = Half.new
+    array     = [2, 5, 6, 8]
+    expected  = test1.half(array)
+
+    expect(expected).to eq [[2, 5][6, 8]]
+  end
 end
