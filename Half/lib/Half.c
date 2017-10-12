e208aea1d4be3528e95eca862e20926f9ce9afdc
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-10-12 15:06:20 -0600

    adds start to half method in c, next step is to call the method in ruby and verify array is passing correctly

---
+++ Half/lib/Half.c
@@ -0,0 +1,15 @@
+#include "ruby.h"
+
+VALUE Half = Qnil;
+void Init_half();
+VALUE method_half(VALUE self, VALUE anObject)
+
+void Init_half() {
+  Half = rb_define_module("Half");
+  rb_define_method(Half, "half", method_half, 1);
+}
+
+VALUE method_half(VALUE self, VALUE inputArr) {
+  int N = RARRAY_LEN(inputArr);
+  printf(N)
+}
